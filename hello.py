bef1ea368850718fb2722dd98b145ee663d65b51
danieloselu3 <doselu3@gmail.com>
2019-01-21 12:44:55 +0300

    added flash messaging

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
 from flask_moment import Moment
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
@@ -23,18 +23,17 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html')
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you changed your name!')
+        session['name'] = form.name.data
         form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 if __name__ == '__main__':
     app.run(debug=True)
